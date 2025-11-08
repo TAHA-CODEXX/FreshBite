ca4036175d8509ecb2af27e1739bf7037dce01cd
TAHA-CODEXX <tahastudying@gmail.com>
2025-11-08 01:37:26 +0100

    Add a condition to check if ther is a number in the name and in the message

--- freshbite.js
+++ freshbite.js
@@ -14,30 +14,37 @@ document.getElementById("envoibtn").addEventListener('click', function() {
   }else if(!eInput.value.endsWith('@gmail.com')){
     eMessage.textContent = 'Email must end with @Gmail.com'
     eMessage.style.display = 'block'
+ }else if(/\d/.test(eInput.value)){
+   eMessage.textContent = 'Please enter your Email'
  }else {
     eMessage.style.display = 'none'
-    eInput.value = ""
     c++
   }
   if (nInput.value === "") {
     nMessage.textContent = 'Please enter your Name'
     nMessage.style.display = 'block'
-  } else {
+  } else if(/\d/.test(nInput.value)){
+    nMessage.textContent = 'Its Not A Name'
+  }else {
     nMessage.style.display = 'none'
-    nInput.value =""
     c++
   }
   if (mInput.value === "") {
     mMessage.textContent = 'Please enter your Message'
     mMessage.style.display = 'block'
-  } else {
+  } else if(/\d/.test(mInput.value)){
+    mMessage.textContent = 'Give us a text message to help you'
+  }else {
     mMessage.style.display = 'none'
-    mInput.value = ""
+    
     c++
   }
   if(c==3){
     cnfrMessage.style.display = 'block'
     inpts.forEach(input => {input.style.marginBottom = '20px'} )
+    mInput.value = ""
+    nInput.value =""
+    eInput.value = ""
    }else{
     cnfrMessage.style.display = 'none'
     inpts.forEach(input => {input.style.marginBottom = '5px'} )
